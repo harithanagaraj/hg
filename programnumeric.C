93e7fa4891983549d1dbb3b5d0de97254dbb4bb7
harithanagaraj <36696201+harithanagaraj@users.noreply.github.com>
2018-03-28 15:59:58 +0530

    prgm

---
+++ programnumeric.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<conio.h>
+ 
+int main()
+{
+  int n, sum = 0, c, value;
+ 
+  printf("How many numbers you want to add?\n");
+  scanf("%d", &n);
+ 
+  printf("Enter %d integers\n", n);
+ 
+  for (c = 1; c <= n; c++)
+  {
+    scanf("%d", &value);
+    sum = sum + value;
+    
+    int(i=0;i++;i>7);
+  }
+ 
+  printf("Sum of the integers = %d\n", sum);
+ 
+  return 0;
